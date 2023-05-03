c352d9be6fe3a02e2a1c17093f33226ebb39009c
Justin Stoecker <justoeck@microsoft.com>
2023-05-03 13:41:36 -0700

    asdf

--- TestOrtCsharp/Program.cs
+++ TestOrtCsharp/Program.cs
@@ -1,34 +1,29 @@
 ﻿using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
+using System.Net.Mime;
 
 public class Program
 {
-    static void Main(string[] args)
+    static void RunModel<T>(DenseTensor<T> aTensor, DenseTensor<T> bTensor, int[] shape, bool useCpu = false)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
-
         var sessionOptions = new SessionOptions();
         sessionOptions.GraphOptimizationLevel = GraphOptimizationLevel.ORT_ENABLE_ALL;
         sessionOptions.EnableMemoryPattern = false;
-        sessionOptions.AppendExecutionProvider_DML();
-        //sessionOptions.AppendExecutionProvider_CPU();
+        if (useCpu)
+        {
+            sessionOptions.AppendExecutionProvider_CPU();
+        }
+        else
+        {
+            sessionOptions.AppendExecutionProvider_DML();
+        }
 
         var session = new InferenceSession("models/add_fp32.onnx", sessionOptions);
 
-        // create CPU input tensors
-        int[] shape = { 3 };
-
-        var aBuffer = new float[] { 0.12341f, 0.32f, -0.05f };
-        var aTensor = new DenseTensor<float>(aBuffer, shape);
-
-        var bBuffer = new float[] { 0.05124f, -0.12f, -0.0345f };
-        var bTensor = new DenseTensor<float>(bBuffer, shape);
-
         var inputs = new List<NamedOnnxValue>
         {
-            NamedOnnxValue.CreateFromTensor<float>("A", aTensor),
-            NamedOnnxValue.CreateFromTensor<float>("B", bTensor),
+            NamedOnnxValue.CreateFromTensor<T>("A", aTensor),
+            NamedOnnxValue.CreateFromTensor<T>("B", bTensor),
         };
 
         var output = session.Run(inputs);
@@ -38,6 +33,25 @@ public class Program
         for (int i = 0; i < shape[0]; i++)
         {
             Console.WriteLine(yTensor[i]);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        bool useFp16 = args.Length > 0 && args[0] == "fp16";
+
+        int[] shape = { 3 };
+        var aBuffer = new float[] { 0.12341f, 0.32f, -0.05f };
+        var bBuffer = new float[] { 0.05124f, -0.12f, -0.0345f };
+
+        if (useFp16)
+        {
+        }
+        else
+        {
+            var aTensor = new DenseTensor<float>(aBuffer, shape);
+            var bTensor = new DenseTensor<float>(bBuffer, shape);
+            RunModel<float>(aTensor, bTensor, shape);
         }
     }
 }
