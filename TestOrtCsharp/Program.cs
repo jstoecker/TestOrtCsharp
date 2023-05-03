5fcfb6434d9aa58c7238c00a8e1bcfc8c5af411e
Justin Stoecker <justoeck@microsoft.com>
2023-05-03 13:21:05 -0700

    Add project files.

---
+++ TestOrtCsharp/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
